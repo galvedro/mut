d4a4d8381b0b5fd692d8b325c75fb3fd6a70317a
Anton Gomez Alvedro <galvedro@gmail.com>
2009-10-18 19:43:26 +0200

    Adding support for segfault capture and testsuite names

--- mut.h
+++ mut.h
@@ -1,76 +1,116 @@
+/* -------------------------------------------------------------------------- */
+/*                                                                            */
+/*                         Micro Unit Testing Framework                       */
+/*                       Copyright (c) 2009, Anton Gomez                      */
+/*                                                                            */
+/* 1. LICENSE                                                                 */
+/*                                                                            */
+/* You are free to use, modify and redistribute this software at will for     */
+/* both commercial and non commercial applications. Just be sure to keep the  */
+/* copyright notice in place.                                                 */
+/*                                                                            */
+/*                                                                            */
+/* 2. USAGE                                                                   */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/* -------------------------------------------------------------------------- */
+
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
 
 
 #ifndef __MUT_H__
 #define __MUT_H__
 
 
-#define TESTSUITE_BEGIN \
-    \
-    enum veredict { \
-        FAILED, \
-        UNDEFINED, \
-    }; \
-    \
-    enum veredict __veredict__ = UNDEFINED; \
-    int __total_tests__ = 0; \
-    int __tests_run__ = 0; \
-    \
-    int main(void) \
-    {
+
+#define TESTSUITE_BEGIN                                                        \
+                                                                               \
+    enum __veredict_t__ {                                                      \
+        __V_FAILED__,                                                          \
+        __V_UNDEFINED__,                                                       \
+    };                                                                         \
+                                                                               \
+    enum __veredict_t__ __veredict__ = __V_UNDEFINED__;                        \
+    int __total_tests__ = 0;                                                   \
+    int __tests_run__ = 0;                                                     \
+    char * __current_test_name__ = "undefined";                                \
+                                                                               \
+    void __sig_handler(int sig)                                                \
+    {                                                                          \
+        __veredict__ = __V_FAILED__;                                           \
+        printf("Error: Test \"%s\" produced a Segmentation Fault!\n",          \
+            __current_test_name__);                                            \
+        printf("-- Testsuite FAILED --\n");                                    \
+        exit(1);                                                               \
+    }                                                                          \
+                                                                               \
+    int main(void)                                                             \
+    {                                                                          \
+        signal(SIGSEGV, __sig_handler);                                        \
 
 
 
-#define TESTSUITE_END \
-__testsuite_end__: \
-        if(__veredict__ == UNDEFINED) \
-        { \
-            printf("-- Testsuite PASSED, %d out of %d tests run --\n", \
-                __tests_run__, __total_tests__); \
-            return 0; \
-        } \
-        else \
-        { \
-            printf("-- Testsuite FAILED, %d out of %d tests run --\n", \
-                __tests_run__, __total_tests__); \
-            return 1; \
-        } \
+#define TESTSUITE_END                                                          \
+__testsuite_end__:                                                             \
+        if(__veredict__ == __V_UNDEFINED__)                                    \
+        {                                                                      \
+            printf("-- Testsuite PASSED, %d out of %d tests run --\n",         \
+                __tests_run__, __total_tests__);                               \
+            return 0;                                                          \
+        }                                                                      \
+        else                                                                   \
+        {                                                                      \
+            printf("-- Testsuite FAILED, %d out of %d tests run --\n",         \
+                __tests_run__, __total_tests__);                               \
+            return 1;                                                          \
+        }                                                                      \
     }
 
 
 
-#define TEST_BEGIN(__test_name__) \
-        { \
-            __label__ __test_end__; \
-            __total_tests__++; \
-            \
-            if (__veredict__ == UNDEFINED) \
-            { \
+#define TEST_BEGIN(__test_name__)                                              \
+        {                                                                      \
+            __label__ __test_end__;                                            \
+            __current_test_name__ = __test_name__;                             \
+            __total_tests__++;                                                 \
+                                                                               \
+            if (__veredict__ == __V_UNDEFINED__)                               \
+            {                                                                  \
                 __tests_run__++;
 
 
 
-#define TEST_END \
-        __test_end__: \
-            ; \
-            } \
+#define TEST_END                                                               \
+        __test_end__:                                                          \
+            ;                                                                  \
+            }                                                                  \
         }
 
 
-#define __ASSERTION_FAILED__ \
-        __veredict__ = FAILED; \
-        printf("ERROR: Assertion failed at line %d\n", __LINE__); \
+
+#define __ASSERTION_FAILED__                                                   \
+        __veredict__ = __V_FAILED__;                                           \
+        printf("Error: Test \"%s\" failed at line %d\n",                       \
+            __current_test_name__, __LINE__);                                  \
         goto __test_end__;
 
 
 
-#define CHECK(__assertion__) \
-    if ( !(__assertion__) ) \
-    { \
-        __ASSERTION_FAILED__ \
+#define CHECK(__assertion__)                                                   \
+    if ( !(__assertion__) )                                                    \
+    {                                                                          \
+        __ASSERTION_FAILED__                                                   \
     }
-    
-    
 
 
 

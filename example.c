a7ddd40bc82a1c470f2cc2c26658a47c1a96549c
Anton Gomez Alvedro <galvedro@gmail.com>
2015-06-08 21:13:16 +0200

    Reorganizing usage information

--- example.c
+++ example.c
@@ -1,3 +1,12 @@
+/*
+ * To try this example, just compile it:
+ * gcc -o example example.c
+ *
+ * and run it:
+ * ./example
+ * -- Testsuite PASSED, 2 out of 2 tests run --
+ */
+
 #include "mut.h"
 
 
--- mut.h
+++ mut.h
@@ -1,9 +1,9 @@
 /* -------------------------------------------------------------------------- */
 /*                                                                            */
-/*                         Micro Unit Testing Framework                       */
-/*                    Copyright (c) 2009, Anton Gomez Alvedro                 */
+/*                        Micro Unit Testing Framework                        */
+/*                  Copyright (c) 2009, Anton Gomez Alvedro                   */
 /*                                                                            */
-/* 1. LICENSE                                                                 */
+/* LICENSE                                                                    */
 /*                                                                            */
 /* Permission is hereby granted, free of charge, to any person obtaining a    */
 /* copy of this software and associated documentation files (the "Software"), */
@@ -22,65 +22,6 @@
 /* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    */
 /* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        */
 /* DEALINGS IN THE SOFTWARE.                                                  */
-/*                                                                            */
-/*                                                                            */
-/* 2. USAGE                                                                   */
-/*                                                                            */
-/* In order to use this unit testing framework you just need to include the   */
-/* header file. Mut test modules have the following structure:                */
-/*                                                                            */
-/* +----------------------------------+                                       */
-/* |                                  |                                       */
-/* | #include <mut.h>                 |                                       */
-/* | #include <your_library.h>        |                                       */
-/* |                                  |                                       */
-/* |                                  |                                       */
-/* | TESTSUITE_BEGIN                  |                                       */
-/* |                                  |                                       */
-/* |                                  |                                       */
-/* | TEST_BEGIN("trivial test")       |                                       */
-/* | ...                              |                                       */
-/* | CHECK( 2 + 2 == 4 );             |                                       */
-/* | ...                              |                                       */
-/* | TEST_END                         |                                       */
-/* |                                  |                                       */
-/* |                                  |                                       */
-/* | TEST_BEGIN("other test")         |                                       */
-/* | TEST_DISABLE                     |                                       */
-/* | ...                              |                                       */
-/* | FAIL_IF( 2 + 2 != 4 );           |                                       */
-/* | ...                              |                                       */
-/* | TEST_END                         |                                       */
-/* |                                  |                                       */
-/* |                                  |                                       */
-/* | TESTSUITE_END                    |                                       */
-/* |                                  |                                       */
-/* +----------------------------------+                                       */
-/*                                                                            */
-/* First of all: test code is written in regular C.                           */
-/*                                                                            */
-/* TESTSUITE_BEGIN and TESTSUITE_END macros are required and provide a main   */
-/* C function with the necessary structure to execute your tests and report   */
-/* results. By compiling this module and linking to the library under test    */
-/* you'll get an executable that will run all tests defined from start to     */
-/* finish and generate a final report on screen.                              */
-/*                                                                            */
-/* TEST_BEGIN and TEST_END serve the purpose of delimiting and naming your    */
-/* test code. It is recommended that your testsuite is divided into small     */
-/* test cases that verify just one thing and that do not depend on preceding  */
-/* tests. In order to accomplish these goals, your tests have to be written   */
-/* in such a way that it should be possible to reorganize the test cases      */
-/* (reordering them) without affecting the final result. If you don't do it   */
-/* like this, it will not be possible to disable individual tests for         */
-/* debugging, or a failure in one test case will generate cascaded errors in  */
-/* subsequent cases. The ultimate goal is that a failure in one test case     */
-/* will spot one and only one broken functionality.                           */
-/*                                                                            */
-/* CHECK() and FAIL_IF() are the assertion macros available in Mut. They      */
-/* are used to explicitly state conditions that have to be verified.          */
-/*                                                                            */
-/* TEST_DISABLE can be used immediately after a TEST_CASE() declaration       */
-/* to quickly disable a test case.                                            */
 /*                                                                            */
 /* -------------------------------------------------------------------------- */
 
